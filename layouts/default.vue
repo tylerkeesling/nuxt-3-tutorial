dd9f8a3c6b86cd58bdc8c5cb8f8187fc8ad2d53d
Tyler Keesling <tyler.keesling@okta.com>
2023-06-09 18:07:14 -0400

    Login works

--- layouts/default.vue
+++ layouts/default.vue
@@ -4,6 +4,9 @@
       <nav class="container mx-auto p-4 flex justify-between">
         <NuxtLink to="/" class="font-bold">Nuxt Dojo</NuxtLink>
         <ul class="flex gap-4">
+          <li><NuxtLink v-if="!isAuthenticated" @click="loginWithRedirect">Login</NuxtLink></li>
+          <li><NuxtLink v-if="isAuthenticated" @click="logOut">Logout</NuxtLink></li>
+          <li><NuxtLink to="/">Home</NuxtLink></li>
           <li><NuxtLink to="/">Home</NuxtLink></li>
           <li><NuxtLink to="/about">About</NuxtLink></li>
           <li><NuxtLink to="/products" class="btn">Merch</NuxtLink></li>
@@ -16,8 +19,18 @@
   </div>
 </template>
 
+<script setup>
+import { useAuth0 } from '@auth0/auth0-vue'
+
+const auth0 = useAuth0()
+const { isAuthenticated, loginWithRedirect, logout } = auth0
+
+const login = () => loginWithRedirect()
+const logOut = () => logout()
+</script>
+
 <style scoped>
-  .router-link-exact-active {
-    color: #12b488;
-  }
+.router-link-exact-active {
+  color: #12b488;
+}
 </style>

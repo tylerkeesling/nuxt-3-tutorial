be26943e7bd3e60390aad7753c44a8cd93fa213c
Tyler Keesling <tyler.keesling@okta.com>
2023-06-09 17:23:37 -0400

    Changing rendering mode to ssr false

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,10 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
   modules: ['@nuxtjs/tailwindcss'],
+  ssr: false,
   app: {
     head: {
-      title: 'Nuxt Dojo',
+      title: 'Nuxt Auth0',
       meta: [{ name: 'description', content: 'Everything about Nuxt 3' }],
       link: [
         { rel: 'stylesheet', href: 'https://fonts.googleapis.com/icon?family=Material+Icons' },

ae7339f1f6ff1e45b216f8984f534c869c4d974e
Tyler Keesling <tyler.keesling@okta.com>
2023-06-10 12:49:51 -0400

    Fixing currency api key

--- server/api/currency/[code].js
+++ server/api/currency/[code].js
@@ -1,7 +1,7 @@
 export default defineEventHandler(async (event) => {
 
   const { code } = event.context.params
-  const { currencyKey } = useRuntimeConfig()
+  const { currencyKey } = useAppConfig()
 
   const uri = `https://api.currencyapi.com/v3/latest?currencies=${code}&apikey=${currencyKey}`
   

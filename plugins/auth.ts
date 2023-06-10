37521127954cf1cc240fe4f474c98ba1ca6e22dd
Tyler Keesling <tyler.keesling@okta.com>
2023-06-10 17:51:54 -0400

    fixing $auth

--- plugins/auth.ts
+++ plugins/auth.ts
@@ -1,4 +1,4 @@
-import { createAuth0 } from '@auth0/auth0-vue';
+import { createAuth0, authGuard } from '@auth0/auth0-vue';
 
 export default defineNuxtPlugin((nuxtApp) => {
   const { domain, clientId, audience, redirectUri: redirect_uri } = useAppConfig().auth;
@@ -18,20 +18,20 @@ export default defineNuxtPlugin((nuxtApp) => {
     nuxtApp.vueApp.use(auth0);
   }
 
-  // addRouteMiddleware('auth', authGuard);
+  addRouteMiddleware('auth', authGuard);
 
   // Basically this under the hood
-  addRouteMiddleware('auth', () => {
-    if (process.client) {
-      auth0?.checkSession();
-      if (!auth0?.isAuthenticated.value) {
-        auth0?.loginWithRedirect({
-          appState: {
-            // Target path the app gets routed to after handling the callback from Auth0 (defaults to '/')
-            target: useRoute().path,
-          },
-        });
-      }
-    }
-  });
+  // addRouteMiddleware('auth', () => {
+  //   if (process.client) {
+  //     auth0?.checkSession();
+  //     if (!auth0?.isAuthenticated.value) {
+  //       auth0?.loginWithRedirect({
+  //         appState: {
+  //           // Target path the app gets routed to after handling the callback from Auth0 (defaults to '/')
+  //           target: useRoute().path,
+  //         },
+  //       });
+  //     }
+  //   }
+  // });
 });

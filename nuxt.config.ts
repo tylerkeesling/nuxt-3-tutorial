d295c12eff5be16424c8b28946e08e7cf1ce7810
Tyler Keesling <tyler.keesling@okta.com>
2023-06-11 14:36:20 -0400

    Remove old dep; install nuxt devtools

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,7 @@
 export default defineNuxtConfig({
   modules: ['@nuxtjs/tailwindcss'],
   telemetry: false,
+  devtools: { enabled: true },
   routeRules: {
     '/': { ssr: true },
     '/protected': { ssr: false },

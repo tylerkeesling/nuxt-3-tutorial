7c9fbb9d08a9422023a1aecd75269e259e7de4c6
Tyler Keesling <tyler.keesling@okta.com>
2023-06-11 14:13:39 -0400

    Deactivate telemetry

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,7 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
   modules: ['@nuxtjs/tailwindcss'],
+  telemetry: false,
   routeRules: {
     '/': { ssr: true },
     '/protected': { ssr: false },
